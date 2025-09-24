616418914ca8a803842e5b6fc257f5ead564a532
karuso356 <gabriel.vas356@gmail.com>
2025-09-24 17:24:42 -0500

    Add files via upload

---
+++ db.py
@@ -0,0 +1,23 @@
+from sqlmodel import SQLModel, create_engine
+
+# Datos de conexión (ajusta según tu RDS)
+DB_USER = "admin1"          # tu usuario de RDS
+DB_PASSWORD = "Elefante123" # tu contraseña de RDS
+DB_NAME = "database1"      # nombre de la base de datos creada en RDS
+DB_HOST = "database1.cn62i4ik22wg.us-east-2.rds.amazonaws.com"
+DB_PORT = "5432"           # 5432 para PostgreSQL, 3306 si usas MySQL
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Crea el engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+    yield
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
---
+++ main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, select
+from typing import Optional, List
+from db import engine, create_db_and_tables, SessionDep  # <--- importamos aquí
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "API corriendo en EC2 🚀 con RDS PostgreSQL"}
+
+
+@app.get("/check-db")
+def check_db(session,SessionDep):
+    result - session.exec(select()).first()
+    return{"db_status":result}
+
+
+# -------- MODELOS --------
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    email: str
+
+class Book(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    author: str
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.post("/users/", response_model=User)
+def create_user(user: User):
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return user
+
+@app.get("/users/", response_model=List[User])
+def list_users():
+    with Session(engine) as session:
+        return session.exec(select(User)).all()
+
+
+@app.post("/books/", response_model=Book)
+def create_book(book: Book):
+    with Session(engine) as session:
+        session.add(book)
+        session.commit()
+        session.refresh(book)
+        return book
+
+@app.get("/books/", response_model=List[Book])
+def list_books():
+    with Session(engine) as session:
+        return session.exec(select(Book)).all()
