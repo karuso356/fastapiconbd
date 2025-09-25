efa62f6cd961d65404caa328c63beb4f43a4f40c
karuso356 <gabriel.vas356@gmail.com>
2025-09-24 19:34:27 -0500

    Update db.py

--- db.py
+++ db.py
@@ -14,10 +14,11 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-    yield
+    
 
 def get_session():
     with Session(engine) as session:
         yield session
+
 
 SessionDep = Annotated[Session, Depends(get_session)]
